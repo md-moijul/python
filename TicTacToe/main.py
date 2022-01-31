5709e23776d91cf4243baa5b423633b18f64bda5
md-moijul <jkmoijul@gmail.com>
2022-01-31 23:12:46 +0000

    implemented smooth scrolling between window

--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -1,5 +1,6 @@
 from tkinter import *
-import gameplay
+
+
 root = Tk()
 
 root.title('tictactoe')
@@ -10,27 +11,65 @@ root.configure(bg="red")
 frame = LabelFrame(root,borderwidth = 1, bg="orange", padx=5, pady=5)
 frame.grid(row=1, column=0,columnspan=6, padx=5, pady=5)
 
-
-def mode():
-    
+def human():
     for widgets in frame.winfo_children():
         widgets.destroy()
-            
-    easy_button = Button(frame, text="EASY",bg="red",width=50 , command=gameplay.easy).grid(row =0 , column=0, pady =10)
-    normal_button = Button(frame, text="NORMAL",bg="red",width=50, command=gameplay.normal).grid(row =1 , column=0, pady=10)
-    easy_button = Button(frame, text="HARD",bg="red",width=50, command=gameplay.hard).grid(row =2 , column=0 ,pady= 10)
+    
+    
+    
+    
+    
+    reset_button = Button(frame, text="reset",bg="orange",width=20 , command= welcome).grid(row =10 , column=0)    
 
-welcome_label =Label(frame, text="welcome to Tic Tac Toe",font=("Times New Roman", 29),bg="red") 
-welcome_label.grid(row=0,column=0,columnspan= 3,pady=(0,20))
+def easy():
+    for widgets in frame.winfo_children():
+        widgets.destroy()
+        
+        
+    reset_button = Button(frame, text="reset",bg="orange",width=20 , command= welcome).grid(row =10 , column=0)    
+    
+    
+def normal():
+    for widgets in frame.winfo_children():
+        widgets.destroy()
+    
+    
+    
+    reset_button = Button(frame, text="reset",bg="orange",width=20 , command= welcome).grid(row =10 , column=0)    
 
-play_as_label = Label(frame , text="play VS :",bg="orange",padx=1).grid(row =1 , column=0)
+def hard():
+    for widgets in frame.winfo_children():
+        widgets.destroy()
+    
+    
+    
+    
+    reset_button = Button(frame, text="reset",bg="orange",width=20 , command= welcome).grid(row =10 , column=0)    
 
-def clear_frame():
-       for widgets in frame.winfo_children():
+def welcome():
+    for widgets in frame.winfo_children():
+        widgets.destroy()
+    def mode():
+        for widgets in frame.winfo_children():
             widgets.destroy()
+            
+        easy_button = Button(frame, text="EASY",bg="red",width=50 , command=easy).grid(row =0 , column=0, pady =10)
+        normal_button = Button(frame, text="NORMAL",bg="red",width=50, command=normal).grid(row =1 , column=0, pady=10)
+        easy_button = Button(frame, text="HARD",bg="red",width=50, command=hard).grid(row =2 , column=0 ,pady= 10)
 
-Human_button = Button(frame, text="Human",bg="orange",width=20 , command= gameplay.human).grid(row =1 , column=1)
-Computer_button = Button(frame, text="Computer",bg="orange",width=20, command= mode).grid(row =1 , column=2)
+    welcome_label =Label(frame, text="welcome to Tic Tac Toe",font=("Times New Roman", 29),bg="red") 
+    welcome_label.grid(row=0,column=0,columnspan= 3,pady=(0,20))
+
+    play_as_label = Label(frame , text="play VS :",bg="orange",padx=1).grid(row =1 , column=0)
+
+    def clear_frame():
+           for widgets in frame.winfo_children():
+                widgets.destroy()
+
+    Human_button = Button(frame, text="Human",bg="orange",width=20 , command= human).grid(row =1 , column=1)
+    Computer_button = Button(frame, text="Computer",bg="orange",width=20, command= mode).grid(row =1 , column=2)
+
+welcome()
 
 
 root.mainloop()
