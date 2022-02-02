fce5f3fb63228df2169a6a35a186a9046c91ddee
md-moijul <jkmoijul@gmail.com>
2022-02-02 11:13:02 +0000

    implemented computer easy level

--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -1,6 +1,8 @@
-from cProfile import label
+from symtable import Symbol
 from tkinter import *
 import copy
+import random
+
 
 root = Tk()
 
@@ -8,9 +10,14 @@ root.title('tictactoe')
 root.geometry("500x500")
 root.configure(bg="red")
 
-symble ="X"
-turn=1
+
 numbers = ["1","2","3","4","5","6","7","8","9"]
+symble ="X"
+type = 0
+move_no= 0
+
+
+
 
 frame = LabelFrame(root,borderwidth = 1, bg="orange", padx=5, pady=5)
 frame.grid(row=2, column=0,columnspan=6, padx=5, pady=5)
@@ -19,27 +26,56 @@ frame1 = LabelFrame(root,borderwidth = 1, bg="orange", padx=5, pady=5)
 frame1.grid(row=1, column=0,columnspan=6, padx=5, pady=5)
         
 def pressed(value):
-    
-    global symble
+    global symble , move_no
     if value.isdigit() is True:
         numbers[numbers.index(value)] = symble
-        
+        move_no += 1
         if symble == "X" :
             symble = "O"
         else :
             symble = "X"
-        board()
+         
+    if type != 0  and move_no < 9:
+        if win_check() == False:
+            compmove()
         
-    else :
-        print(value)
+    board()
         
+def compmove():
+    global symble, move_no
+    if type == 1:
+        choice = 0
+        while numbers[choice].isdigit() is False:
+            choice = (random.randint(0, 8))
+        
+        print(choice+1)   
+        numbers[choice] = symble
+        move_no += 1
+        symble = "X"
+        
+
+            
+def reset():
     
+        for widgets in frame.winfo_children():
+            widgets.destroy()
+        for widgets in frame1.winfo_children():
+            widgets.destroy()
+            
+        global numbers, symble, move_no, type
+        numbers = ["1","2","3","4","5","6","7","8","9"]
+        symble ="X"
+        type = 0
+        move_no= 0
+        welcome()
+        
+
 def win_check():
     if numbers[0]==numbers[1] and numbers[1]==numbers[2]:
         return True
     elif numbers[3]==numbers[4] and numbers[4]==numbers[5]:
         return True
-    elif numbers[6]==numbers[7] and numbers[7]==numbers[9]:
+    elif numbers[6]==numbers[7] and numbers[7]==numbers[8]:
         return True
     elif numbers[0]==numbers[3] and numbers[3]==numbers[6]:
         return True
@@ -55,35 +91,55 @@ def win_check():
         return False
     
 def status():
-
     for widgets in frame1.winfo_children():
             widgets.destroy()
     
-    if symble == "X":
-        player_no= "1"
-    else:
-        player_no= "2" 
-        
-    if symble == "X":
-        winner_no= "2"
-    else:
-        winner_no= "1" 
-       
-    textstatus =("Player no "+ player_no +" make your move :")
-    winstatus=("Player no "+ winner_no +"\nwon the match")
-    
-    if win_check() == False:
-        status_label =Label(frame1, text=textstatus,font=("Times New Roman", 15), bg="orange") 
-        status_label.grid(row=9,column=0,columnspan= 5)
-    else :
-        for widgets in frame.winfo_children():
-            widgets.destroy()
+    if type == 0:
+        if symble == "X":
+            player_no= "1"
+        else:
+            player_no= "2" 
             
-        status_label =Label(frame1, text=winstatus,font=("Times New Roman", 50), bg="orange") 
-        status_label.grid(row=9,column=0,columnspan= 5)
-    
-    
-
+        if symble == "X":
+            winner_no= "2"
+        else:
+            winner_no= "1" 
+        
+        textstatus =("Player no "+ player_no +" make your move :")
+        winstatus=("Player no "+ winner_no +"\nwon the match")
+        
+        if win_check() == False:
+            status_label =Label(frame1, text=textstatus,font=("Times New Roman", 15), bg="orange") 
+            status_label.grid(row=9,column=0,columnspan= 5)
+        else :
+            for widgets in frame.winfo_children():
+                widgets.destroy()
+                
+            status_label =Label(frame1, text=winstatus,font=("Times New Roman", 50), bg="orange") 
+            status_label.grid(row=9,column=0,columnspan= 5)
+    else:
+        if symble == "X":
+            winner_no= "computer"
+        else:
+            winner_no= "you"
+            
+        compstatus= ("it's your move")
+        drawstatus="the match has been drawn"
+        winstatus =(winner_no+" won the match!")
+        
+        if win_check() == False:
+            if move_no < 9:
+                status_label =Label(frame1, text=compstatus,font=("Times New Roman", 20), bg="orange") 
+                status_label.grid(row=9,column=0,columnspan= 5)
+            else:
+                status_label =Label(frame1, text="match has been drawn",font=("Times New Roman", 20), bg="orange") 
+                status_label.grid(row=9,column=0,columnspan= 5)
+        else:
+            for widgets in frame.winfo_children():
+                widgets.destroy()
+             
+            status_label =Label(frame1, text=winstatus,font=("Times New Roman", 30), bg="orange") 
+            status_label.grid(row=9,column=0,columnspan= 5)    
 def board():
     for widgets in frame.winfo_children():
         widgets.destroy()
@@ -158,7 +214,7 @@ def board():
     
     
     status()
-    reset_button = Button(frame, text="reset",bg="orange",width=20 , command= welcome).grid(row =10 , column=0, columnspan=6) 
+    reset_button = Button(frame, text="reset",bg="orange",width=20 , command= reset).grid(row =10 , column=0, columnspan=6) 
   
 
 def human():
@@ -168,41 +224,44 @@ def human():
     board()
     
 
-def easy():
+def computer(lavel):
+    global type
+    
+    
+    if lavel == 1:
+        type = 1
+    elif lavel == 2:
+        type = 2
+    elif lavel == 3:
+        type = 3
+    else:
+        type = 0
+    
     for widgets in frame.winfo_children():
         widgets.destroy()
         
     board()
   
-    
-def normal():
-    for widgets in frame.winfo_children():
-        widgets.destroy()
-    
-    board()   
-    
-
-def hard():
-    for widgets in frame.winfo_children():
-        widgets.destroy()
-    
-    board()    
+       
     
 
 def welcome():
     global numbers
-    numbers = ["1","2","3","4","5","6","7","8","9"]
     for widgets in frame.winfo_children():
+        widgets.destroy()
+    for widgets in frame1.winfo_children():
         widgets.destroy()
     def mode():
         for widgets in frame.winfo_children():
             widgets.destroy()
         for widgets in frame1.winfo_children():
             widgets.destroy()
-            
-        easy_button = Button(frame, text="EASY",bg="red",width=50 , command=easy).grid(row =0 , column=0, pady =10)
-        normal_button = Button(frame, text="NORMAL",bg="red",width=50, command=normal).grid(row =1 , column=0, pady=10)
-        easy_button = Button(frame, text="HARD",bg="red",width=50, command=hard).grid(row =2 , column=0 ,pady= 10)
+        
+        info_label =Label(frame1, text="choose the difficulty level :",font=("Times New Roman", 20),bg="orange") 
+        info_label.grid(row=0,column=0,columnspan= 3,pady=(0,20))
+        easy_button = Button(frame, text="EASY",bg="red",width=50 , command=lambda: computer(1)).grid(row =0 , column=0, pady =10)
+        normal_button = Button(frame, text="NORMAL",bg="red",width=50, command=lambda: computer(2)).grid(row =1 , column=0, pady=10)
+        easy_button = Button(frame, text="HARD",bg="red",width=50, command=lambda: computer(3)).grid(row =2 , column=0 ,pady= 10)
 
     welcome_label =Label(frame1, text="welcome to Tic Tac Toe",font=("Times New Roman", 29),bg="red") 
     welcome_label.grid(row=0,column=0,columnspan= 3,pady=(0,20))
